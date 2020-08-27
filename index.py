e131c569b3065bae38220cecfd8fcd91076d797e
italolourenco <italolt10@gmail.com>
2020-08-26 23:35:06 -0300

    feat: import datasets

--- index.py
+++ index.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import matplotlib.pylab as plt
 
-s60DataSet = "data/musicReport-60.csv"
-s70DataSet = "data/musicReport-70.csv"
-s80DataSet = "data/musicReport-80.csv"
-s90DataSet = "data/musicReport-90.csv"
-s2000DataSet = "data/musicReport-2000.csv"
-s2010DataSet = "data/musicReport-2010.csv"
+s60DataSetClear = '/data/clearData/musicReport-ClearData-60.csv'
+s60DataSetStemming = '/data/stemmingData/musicReport-StemmingData-60.csv'
+
+s70DataSetClear = '/data/clearData/musicReport-ClearData-70.csv'
+s70DataSetStemming = '/data/stemmingData/musicReport-StemmingData-60.csv'
+
+s80DataSetClear = '/data/clearData/musicReport-ClearData-80.csv'
+s80DataSetStemming = '/data/stemmingData/musicReport-StemmingData-60.csv'
+
+s90DataSetClear = '/data/clearData/musicReport-ClearData-90.csv'
+s90DataSetStemming = '/data/stemmingData/musicReport-StemmingData-60.csv'
+
+s2000DataSetClear = '/data/clearData/musicReport-ClearData-2000.csv'
+s2000ataSetStemming = '/data/stemmingData/musicReport-StemmingData-60.csv'
+
+s2010DataSetClear = '/data/clearData/musicReport-ClearData-2010.csv'
+s2010ataSetStemming = '/data/stemmingData/musicReport-StemmingData-60.csv'
 
 def main():
 
