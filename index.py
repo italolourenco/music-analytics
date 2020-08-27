1e3b0462d2e9c92dcb99c402c09f1df384e86b65
italolourenco <italolt10@gmail.com>
2020-08-26 23:29:09 -0300

    fist commit

---
+++ index.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+s60DataSet = "data/musicReport-60.csv"
+s70DataSet = "data/musicReport-70.csv"
+s80DataSet = "data/musicReport-80.csv"
+s90DataSet = "data/musicReport-90.csv"
+s2000DataSet = "data/musicReport-2000.csv"
+s2010DataSet = "data/musicReport-2010.csv"
+
+def main():
+
+
+if __name__ == '__main__':
+	main()
