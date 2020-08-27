ec708953354fe29c68c2347f56c244b17603d1c8
italolourenco <italolt10@gmail.com>
2020-08-26 23:38:11 -0300

    feat: list dataset

--- index.py
+++ index.py
@@ -21,6 +21,12 @@ s2010ataSetStemming = '/data/stemmingData/musicReport-StemmingData-60.csv'
 
 def main():
 
+    decades = ['60', '70', '80', '90', '2000', '2010']
+
+    listPathClearData = [s60DataSetClear, s70DataSetClear, s80DataSetClear, s90DataSetClear, s2000DataSetClear, s2010DataSetClear]
+
+    listPathStemmingData = [s60ataSetStemming, s70ataSetStemming, s80ataSetStemming, s90ataSetStemming, s2000ataSetStemming, s2010ataSetStemming]
+
 
 if __name__ == '__main__':
 	main()
